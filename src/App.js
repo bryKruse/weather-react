a6d396b4a281c3c04b8deeb45a45172e5dadc4d3
bryKruse <90730678+bryKruse@users.noreply.github.com>
2022-01-04 09:59:52 -0600

    Added react and axios to vs code and moved weather search bar into VS code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchBar from "./SearchBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <SearchBar />
+        <br />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Open-source
+          <a href="" alt="link to GitHub" target="_blank" rel="noreferrer">
+            code
+          </a>
+          by Brynn Kruse
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
